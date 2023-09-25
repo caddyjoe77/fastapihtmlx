935ab4eb4fd9e575fce63251327b4fda59a37444
Joe M <caddyjoe77@gmail.com>
2023-09-24 20:05:57 -0500

    initial code commit
    nothing fancy -- setting up skeleton

---
+++ main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI()
+
+templates = Jinja2Templates(directory="templates")
+
+@app.get('/index', response_class=HTMLResponse)
+def index(request: Request):
+    films = [
+        {'name': 'Blade Runner', 'director': 'Ridley Scott'},
+        {'name': 'Pulp Fiction', 'director': 'Quentin Tarantino'},
+        {'name': 'Mulholland Drive', 'director': 'David Lynch'},
+    ]
+    context = {'request': request, 'films': films}
+    return templates.TemplateResponse("index.html", context)
